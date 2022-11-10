cdad1ed6d0120a9ded94aa6385417e744fe9e807
jwd-code <jedicki@iu.edu>
2022-11-10 17:38:08 -0500

    Updated maxmatch

--- 01_Tokenisation/Maxmatch.py
+++ 01_Tokenisation/Maxmatch.py
@@ -1,38 +1,41 @@
 import pyconll
 
-path = "C:\\Users\\Jerem\\Desktop\\maxmatch\\ja_gsd-ud-train.conllu"
+train_path = 'C:\\Users\\Jerem\\Downloads\\UD_Japanese-GSD-master\\UD_Japanese-GSD-master\\ja_gsd-ud-train.conllu'
+test_path = 'C:\\Users\\Jerem\\Documents\\Fall 2022\\L-545\\Practicals\\maxmatch\\testfile.txt'
 
-corpus = pyconll.iter_from_file(path)
+corpus = pyconll.iter_from_file(train_path)
 
+dictionary = []
 for sentence in corpus:
-    print(sentence)
+    for token in sentence:
+        dictionary.append(token.form)
+
+sorted_dict = sorted(dictionary, key=len, reverse=True)
 
 
 
 
-# dictionary = []
-# for sentence in corpus:
-#     for token in sentence:
-#         dictionary.append(token.form)
-
-# sorted_dict = sorted(dictionary, key=len, reverse=True)
-
-# training_dict = open('training_dict.txt', 'w', encoding='utf-8')
-
-# for sent in sorted_dict:
-#     training_dict.write(sent)
-#     training_dict.write('\n')
-# training_dict.close()
+test_file = open(test_path, 'r', encoding='utf-8')
 
 
-# def maxmatch(sentence, dictionary):
-#     if len(sentence) == 0:
-#         return []
-#     for i in range(len(sentence), 1, -1):
-#         first_word = sentence[:i]
-#         remainder = sentence[i:]
-#         if first_word in sorted_dict:
-#             return (list(first_word, maxmatch(remainder, dictionary)))
-#     first_word = sentence[:1]
-#     remainder = sentence[1:]
-#     return list(first_word, maxmatch(remainder, dictionary))
+def maxmatch(sentence, dictionary):
+    if len(sentence) == 0:
+        return []
+    for i in range(len(sentence), 1, -1):
+        first_word = sentence[:i]
+        remainder = sentence[i:]
+        if first_word in sorted_dict:
+            first_word = [first_word]
+            return list(first_word) + maxmatch(remainder, dictionary)
+            # return [first_word + [maxmatch(remainder, dictionary)]]
+    first_word = sentence[:1]
+    remainder = sentence[1:]
+    first_word = [first_word]
+
+    return list(first_word) + maxmatch(remainder, dictionary)
+
+for line in test_file:
+    x = (maxmatch(line, sorted_dict))
+    print(x)
+
+print('DONE')
